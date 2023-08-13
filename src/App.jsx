1fd5c29c5eda15cc552523121515c42627be5058
Yasin Cakal <whoisyasin@gmail.com>
2023-08-13 21:51:47 +0300

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { useState } from 'react'
+
 function App() {
+  const [color, setColor] = useState('white')
+
+  const handleColorChange = e => {
+    setColor(e.target.value)
+  }
+
   return (
     <>
-      <p>Hello</p>
+      <h1>Display Colors</h1>
+      <div
+        className='color-box'
+        style={{ backgroundColor: color }}>
+        <p className='color-box-p'>{color}</p>
+      </div>
+      <input
+        className='color-input'
+        type='text'
+        placeholder={color}
+        onChange={handleColorChange}
+      />
     </>
   )
 }
